31a4c20d0bb206ffdc32bc1add2f263536b3a7cc
AMDevG <jeberry308@gmail.com>
2017-10-28 04:26:36 -0500

    Running MinDistance; Need to Return JSON of the Evacuator Object

--- savior_app/urls.py
+++ savior_app/urls.py
@@ -4,6 +4,5 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-   #url(r'^process/(?P<string>[\w\-]+)/$', views.process, name="process")
-   url(r'^process/(?P<name>[\w\-]+)/$', views.process, name="process")
-]
+	url(r'^process/(?P<vicID>[\w\-]+)/(?P<lat>[-+]?([0-9]*\.[0-9]+|[0-9]+))/(?P<lng>[-+]?([0-9]*\.[0-9]+|[0-9]+))/(?P<grpSize>[0-9])/$', views.process, name="process")
+	]
--- savior_app/views.py
+++ savior_app/views.py
@@ -15,17 +15,70 @@ class EvacuatorViewSet(viewsets.ModelViewSet):
 	serializer_class = EvacuatorSerializer
 
 
-def process(request, name):
+def process(request, vicID, lat, lng, grpSize):
+#def process(request, vicID, lat):
+	print("VicID Passed: ", vicID)
+	print("lat Passed: ", lat)
+	print("lng Passed: ", lng)
+	print("grpSize Passed: ", grpSize)
 
+	findNearestEvacuator(grpSize)
 
-	print("Received vicID: ")
-	print("Received vicName: ")
-
-	return HttpResponse("Vic ID: %s"%name)
+	
+	return HttpResponse("Vic ID: %s"%vicID)
 
 
 def calculateDistance(victimCoords, evacCords):
 	return great_circle(victimCoords, evacCords).miles
+
+
+
+def testDB():
+	for evacuator in Evacuator.objects.all():
+		print("Evacuator ", evacuator.evacuatorID)
+
+
+def findNearestEvacuator(grpSize):
+	group_size = int(grpSize)
+	available_evacuators = []
+	min_dist = {}
+
+	victimCoords = (45.679,-51.456)
+
+	## Calculates evacs who have available space
+	for evacuator in Evacuator.objects.all():
+		capacity = int(evacuator.maxCapacity) - int(evacuator.currentCapacity)
+		print("Capacity is ",capacity)
+		if group_size <= capacity:
+			print("Adding to available evacuator")
+			available_evacuators.append(evacuator)
+			
+	for evacuator in available_evacuators:
+		evac_coords = (evacuator.lat, evacuator.lng)
+
+		if bool(min_dist) == False:
+			print("Setting mindist")
+			min_dist[evacuator.evacuatorID] = int(calculateDistance(victimCoords, evac_coords))
+
+		else:
+			print("Not first loop")
+			current_distance = int(calculateDistance(victimCoords, evac_coords))
+
+			for key in min_dist.keys():
+				if current_distance < min_dist[key]:
+					min_dist[evacuator.evacuatorID] = current_distance
+					print("Changin min distance to: ", current_distance)
+
+	print(min_dist)
+
+
+
+
+
+
+
+
+
 
 
 
