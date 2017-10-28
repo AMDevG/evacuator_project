214888bab0e95c93b79c0b331247ebe2c5a7612f
AMDevG <jeberry308@gmail.com>
2017-10-28 01:48:23 -0500

    REST API RUNNING

--- savior_app/models.py
+++ savior_app/models.py
@@ -4,11 +4,11 @@ from django.db import models
 
 class Evacuator(models.Model):
         evacuatorID = models.CharField(max_length=3, null=True, blank=True, default=None)
-        lat = models.CharField(max_length=3, null=True, blank=True, default=None)
-        lng = models.CharField(max_length=3, null=True, blank=True, default=None)
-        currentCapacity = models.CharField(max_length=3, null=True, blank=True, default=None)
-        maxCapacity = models.CharField(max_length=3, null=True, blank=True, default=None)
+        lat = models.CharField(max_length=20, null=True, blank=True, default=None)
+        lng = models.CharField(max_length=20, null=True, blank=True, default=None)
+        currentCapacity = models.CharField(max_length=2, null=True, blank=True, default=None)
+        maxCapacity = models.CharField(max_length=2, null=True, blank=True, default=None)
         active = models.BooleanField(default=True)
 
         def __str__(self):
-            return '{}'.format(self.name)
+            return '{}'.format(self.evacuatorID)
